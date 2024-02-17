51c1c21c17819fb44914a31bcf9d4973a2e5e81c
notetakingforeggs <150667588+notetakingforeggs@users.noreply.github.com>
2024-02-17 12:56:40 +0000

    First commit of archive_stations.py
    just pasting into github to get it into the dir so it can access the db, not yet confirmed to be working

---
+++ archive_stations.py
@@ -0,0 +1,48 @@
+import sqlite3
+from datetime import datetime
+
+def create_table(cursor, table_name):   
+    cursor.execute(f'''
+                    CREATE TABLE IF NOT EXISTS {table_name}(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    foreign_id INTEGER,
+                    city TEXT,
+                    station_id INTEGER,
+                    name TEXT,
+                    latitude REAL,
+                    longitude REAL,
+                    last_checked TEXT,
+                    days_since INTEGER,
+                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    )
+                ''')
+    conn.commit()
+
+def populate_table(cursor, table_name):
+    cursor.execute(f'''
+                    INSERT INTO {table_name}(
+                    foreign_id,
+                    city,
+                    station_id,
+                    name,
+                    latitude,
+                    longitude,
+                    last_checked,
+                    days_since
+                    )
+                    SELECT city,
+                    station_id,
+                    name,
+                    latitude,
+                    longitude,
+                    last_checked,
+                    days_since
+                    FROM stations
+             ''')
+
+conn = sqlite3.connect("stations.db")
+cursor = conn.cursor()
+table_name = "historical_station_checks_data"
+
+create_table(cursor, table_name)
+populate_table(cursor, table_name)
